6193e0218c07ca164edc33d1c30223e0197ffff5
msb217 <mattbuelldev@gmail.com>
2021-07-26 12:13:57 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# TrainingPeaks Bulk Elevation Adjustment Utility
+This utility will select an athlete's activities in TrainingPeaks within a specified date range and apply elevation correction to them.
+
+TrainingPeaks does not support auto-applying elevation adjustment to activities, nor does it support bulk applying elevation correction to many activities at a time.
+
+This script is useful if you have hundreds of activities you'd like to apply elevation correction to and do not want to manually select all of them via the web app.
+
+## Usage
+1. Read the disclaimer below
+2. Sign into TrainingPeaks via your web browser  
+3. Navigate to the 'Network' tab of your browser's dev tools  
+4. Obtain the two request headers `Cookie` and `User-Agent` from a request to `tpapi.trainingpeaks.com`. You may need to check multiple requests for `Cookie`. You will need to update `Cookie` whenever your TrainingPeaks session is closed.
+5. Set `Cookie` and `User-Agent` as environment variables with the names `TRAININGPEAKS_SESSION_COOKIE` and `TRAININGPEAKS_USER_AGENT`
+6. Find a request to `tpapi.trainingpeaks.com` with the path prefix `/fitness/v1/athletes/{athlete_id}`.
+7. Set the numeric `athlete_id` to an environment variable `TRAININGPEAKS_ATHLETE_ID`
+
+```
+➜  python adjust.py --start-date 2021-01-01 --tags running cycling
+
+➜  python adjust.py -h
+usage: adjust.py [-h] --start-date START_DATE [--end-date END_DATE]
+                 [--tags TAGS [TAGS ...]]
+
+A tool to bulk apply elevation correction to TrainingPeaks activities
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+  --start-date START_DATE
+                        Beginning of date range for bulk elevation adjustment (YYYY-mm-dd)
+
+  --end-date END_DATE   End of date range for bulk elevation adjustment (YYYY-mm-dd)
+                        Defaults to current date
+
+  --tags TAGS [TAGS ...]
+                        A list of tags for identifying activities to be
+                        corrected, ex: '--tags running cycling'
+```
+
+## Disclaimer
+Use this tool at your own discretion and risk. I am in no way responsible for any issues this software causes to (but not limited to) your computer or your TrainingPeaks account
+
+## To TrainingPeaks
+If you feel this software violates your ToC, reach out to me and I will happily remove it from github.
---
+++ adjust.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+from datetime import datetime
+
+import requests
+
+parser = argparse.ArgumentParser(description='A tool to bulk apply elevation correction to TrainingPeaks activities')
+parser.add_argument('--start-date', help='Beginning of date range for bulk elevation adjustment', required=True)
+parser.add_argument('--end-date', help='End of date range for bulk elevation adjustment. Defaults to current date',
+                    default=datetime.today().strftime('%Y-%m-%d'))
+parser.add_argument('--tags',
+                    help='A list of tags for identifying activities to be corrected, ex: --tags=running,cycling',
+                    nargs='+', default=[])
+
+args = vars(parser.parse_args())
+
+start_date = args.get('start_date')
+end_date = args.get('end_date')
+tags = set(map(str.lower, args.get('tags')))
+
+athlete_id = os.environ.get('TRAININGPEAKS_ATHLETE_ID')
+user_agent = os.environ.get('TRAININGPEAKS_USER_AGENT')
+cookie = os.environ.get('TRAININGPEAKS_SESSION_COOKIE')
+
+headers = {
+    'User-Agent': user_agent,
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Content-Type': 'application/json',
+    'Origin': 'https://app.trainingpeaks.com',
+    'Connection': 'keep-alive',
+    'Referer': 'https://app.trainingpeaks.com/',
+    'Cookie': cookie,
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'TE': 'trailers'
+}
+
+get_activities = requests.get(
+    'https://tpapi.trainingpeaks.com/fitness/v1/athletes/{athlete_id}/workouts/{start_date}/{end_date}'.format(
+        athlete_id=athlete_id, start_date=start_date, end_date=end_date), headers=headers)
+
+workouts_json = get_activities.json()
+
+runs = []
+
+for workout in workouts_json:
+    if len(tags) > 0:
+        user_tags = workout['userTags']
+        if user_tags is not None:
+            user_tags = set(map(str.lower, user_tags.split(',')))
+            if tags & user_tags:
+                runs.append(workout['workoutId'])
+    else:
+        runs.append(workout['workoutId'])
+
+for run in runs:
+    adjust_elevation = requests.post(
+        'https://tpapi.trainingpeaks.com/groundcontrol/v2/commands/workouts/{run}/applyelevationstofile'.format(
+            run=run), headers=headers, data={})
+
+    print("Activity ID: {run}, Status Code: {status_code}".format(run=run, status_code=adjust_elevation.status_code))
